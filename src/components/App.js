d1a01588291f58c4c4aa07645d39a33075cefdf8
jkkjkhh <jkkjkhh@gmail.com>
2022-08-23 11:48:24 +0900

    fetch를 사용하여 데이터 불러오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
 import { useState } from "react";
+import { getReviews } from "../api";
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -15,11 +15,17 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>베스트순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
