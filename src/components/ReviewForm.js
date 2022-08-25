a813ad1b61ea888b96537d5fc2b2cd1e97dd47d2
jkkjkhh <jkkjkhh@gmail.com>
2022-08-25 11:19:19 +0900

    확인 버튼 생성

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,14 +1,39 @@
 import { useState } from "react";
+import "./ReviewForm.css";
 
 function ReviewForm() {
   const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [content, setContent] = useState("");
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
   };
+
+  const handleRatingChange = (e) => {
+    const nextRating = Number(e.target.value) || 0;
+    setRating(nextRating);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      title,
+      rating,
+      content,
+    });
+  };
+
   return (
-    <form>
-      <input value={title} onChange={handleTitleChange}></input>
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <input value={title} onChange={handleTitleChange} />
+      <input type="number" value={rating} onChange={handleRatingChange} />
+      <textarea value={content} onChange={handleContentChange} />
+      <button type="submit">확인</button>
     </form>
   );
 }
